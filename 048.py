8dfcf8a4952ec1aed88c19c2cf6789922ce59af3
Fernanda Moura <ferferreira199609@gmail.com>
2026-04-04 20:53:46 -0300

    inserção de acumulador e contador

--- 048.py
+++ 048.py
@@ -1,3 +1,11 @@
-for num in range (0,500):
+#essa variavel "soma" refere- se a um acumulador
+soma = 0 # soma um valor acumulado
+contador = 0 #geralmente conta +1
+
+for num in range (1,500 ,2):
     if num % 3 == 0:
-        print(num)
+
+        soma = soma + num
+        contador += 1
+
+print (f'A soma de todos os {contador} números impares, divisiveis por 3 é {soma} ')
