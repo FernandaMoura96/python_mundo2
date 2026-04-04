2f4fd8f9f2fe7e9af786b12c040a8bab9c556d19
Fernanda Moura <ferferreira199609@gmail.com>
2026-04-04 20:59:27 -0300

    Mudança de formatação no cod

--- 048.py
+++ 048.py
@@ -5,7 +5,7 @@ contador = 0 #geralmente conta +1
 for num in range (1,500 ,2):
     if num % 3 == 0:
 
-        soma = soma + num
+        soma  =+ num
         contador += 1
 
 print (f'A soma de todos os {contador} números impares, divisiveis por 3 é {soma} ')
