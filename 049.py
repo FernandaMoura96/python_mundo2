25e0548a5e87f551099a5516ec72ed46abd8372f
Fernanda Moura <ferferreira199609@gmail.com>
2026-04-04 21:25:18 -0300

    correÃeção das f'string

--- 049.py
+++ 049.py
@@ -2,8 +2,8 @@
 # só que agora utilizando um laço for.
 
 n = int(input('Digite um número para ver sua tabuada: '))
-for c in range(1, 10):
-    print(f'{n} X 1 = {n * c}')
+for c in range(1, 11):
+    print(f'{n} X {c} = {n * c}')
 
 
 '''
