2f86025008e36d1f70e464dbefef204fd61dbd5a
Fernanda Moura <ferferreira199609@gmail.com>
2026-04-04 21:15:20 -0300

    049.py

---
+++ 049.py
@@ -0,0 +1,25 @@
+#Exercício Python 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher,
+# só que agora utilizando um laço for.
+
+n = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1, 10):
+    print(f'{n} X 1 = {n * c}')
+
+
+'''
+print(f'{n} X 2 = {n*2}')
+print(f'{n} X 3 = {n*3}')
+print(f'{n} X 4 = {n*4}')
+print(f'{n} X 5 = {n*5}')
+print(f'{n} X 8 = {n*6}')
+print(f'{n} X 7 = {n*7}')
+print(f'{n} X 8 = {n*8}')
+print(f'{n} X 9 = {n*9}')
+print(f'{n} X 10 = {n*10}')
+'''
+
+
+'''Deixei aqui a 'sobra' do exercicio 09 que eu tinha feito antes e já estava no repositório,
+para que fique de exemplo a diferença entre em codigo e outro'''
+
+print('--------------------------')
