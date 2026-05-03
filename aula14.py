fa5a9b933539beb84a5613e24214f6b189aad2d3
Fernanda Moura <ferferreira199609@gmail.com>
2026-05-03 17:26:41 -0300

    Laço while

---
+++ aula14.py
@@ -0,0 +1,13 @@
+'''for c in range (1,11):
+    print(c
+print("fuii")'''
+'''c =1
+while c <11:
+    print (c)
+    c = c + 1
+print("fuii")'''
+
+n=1
+while n != 0:
+     n = int(input('Digite um numero: '))
+print('FIM')
